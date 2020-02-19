b39ff7c6f9a5b75032694f8be4d2931f5a8bcc69
Anthony Nguyen <nguyean6@flip2.engr.oregonstate.edu>
2020-02-18 23:09:15 -0800

    update

--- mstEuclid.py
+++ mstEuclid.py
@@ -19,6 +19,38 @@ def distance(p1,p2):
     return round(total)
 
 
+def mergesort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+        #divide
+        mergesort(left)
+        mergesort(right)
+
+        i = j = 0 #left and right
+        k = 0 # main array
+        #conquer
+        while i < len(left) and j < len(right):
+            if left[i].distance < right[j].distance:
+                array[k] = left[i]
+                i = i + 1
+            else:
+                array[k] = right[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j = j + 1
+            k = k + 1
+
+
 
 # to show command line arguments
 print("Number of arguments:", len(sys.argv), "arguments.")
@@ -30,25 +62,25 @@ text_name = str(sys.argv[1])
 file = open(text_name,"r")
 lines = file.readlines()#reads all the lines in the file
 file.close()
-print(lines)
+#print(lines)
 current_line = 0
 
 temp = [int(i) for i in lines[0].split() if i.isdigit()]
 n_point = temp[0] # number of points
-print (n_point)
+#print (n_point)
 
 # putting the set of points in an array
 points = [] #set of points
 for num in range(n_point):
     points.append([int(i) for i in lines[num+1].split() if i.isdigit()])
-print(points)
+#print(points)
 
 
 
 # making the adjaceny matrix
 adj_matrix = [[0 for i in range(n_point)] for j in range(n_point)]
-for i in range(len(adj_matrix)):
-    print(adj_matrix[i])
+# for i in range(len(adj_matrix)):
+#     print(adj_matrix[i])
 
 #prints out lower trinagle without zeros
 for i in range(len(adj_matrix)):
@@ -60,9 +92,9 @@ for i in range(n_point): # i and j are points
     for j in range(n_point):
         adj_matrix[i][j] = Data(distance(points[i],points[j]),points[i],points[j],key)
         key+=1
-        adj_matrix[i][j].show_all()
+        #adj_matrix[i][j].show_all()
     # print(adj_matrix[i].distance)
-    print("\n")
+    #print("\n")
 
 #put everything in one array then sort, lower triangle
 W = []
@@ -70,9 +102,13 @@ for i in range(len(adj_matrix)): # add weights to lower triangle
     for j in range(len(adj_matrix)-(len(adj_matrix)-i)):
         W.append(adj_matrix[i][j])
 
+# for i in range(len(W)):
+#     W[i].show_all()
 
 
-
+print("")
+for i in range(len(W)):
+    W[i].show_all()
 # a set that contain all edges
 # for each vertex v in G.V(the vertex with):
 #       make a set (v)
@@ -81,12 +117,25 @@ for i in range(len(adj_matrix)): # add weights to lower triangle
 #   A = A union {(u,v)}
 #   union(u,v)
 #return A
-def kruskal():
-    A = [] # contains all edges
+def kruskal(W,points):
+    A = [] # contains all edges,key
     T = [] # contain all points
+    mergesort(W)
+    for edge in range(len(W)):
+        if (len(T) == len(points)) and (T == points): #if all the points are in the tree then break
+            break
+        if W[edge].p1 and W[edge].p2 in T:
+            continue
+        if W[edge].key not in A: # then add node
+            if W[edge].p1 not in T:
+                T.append(W[edge].p1)
+            if W[edge].p2 not in T:
+                T.append(W[edge].p2)
+            A.append(W[edge].key)
+    print ("A: %s"% (A))
+    print ("T: %s" %T)
 
 
-
-
+kruskal(W, points)
 def main():
     pass
